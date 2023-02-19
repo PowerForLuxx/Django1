5a0b2d58ea27ad298f08607446c0b0284fc20655
Aidyn <112602044+PowerForLuxx@users.noreply.github.com>
2023-02-19 18:41:58 +0600

    1
    Models

---
+++ coolsite/kinofilm/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+
+from .views import *
+
+urlpatterns = [
+    path('home/', index, name='home'),
+    path('cats/<int:catid>/', categories),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+]
